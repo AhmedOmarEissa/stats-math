d971d277d92fb9cc1ac25dafe4e805ef04089bf5
AhmedOmarEissa <ahmedomar92@yahoo.com>
2024-12-27 16:17:48 +0000

    add chisquare commented

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ def generate_t_distribution(degrees_of_freedom, num_points):
     # Calculate t-distribution points
     t_points = z_points / np.sqrt(chi_squared_points / degrees_of_freedom)
 
-    return z_points, t_points
+    return z_points, t_points ,chi_squared_points
 
 # Streamlit App Title
 st.title("t-Distribution Generator ")
@@ -36,15 +36,17 @@ num_points = st.sidebar.number_input("Number of points", min_value=100, max_valu
 
 # Generate t-distribution points
 # if st.sidebar.button("Generate t-Distribution"):
-z_distribution, t_distribution_points = generate_t_distribution(degrees_freedom, num_points)
+z_distribution, t_distribution_points , chi_squared_points = generate_t_distribution(degrees_freedom, num_points)
 
 # Display results
 st.write(f"Generated {num_points} points with {degrees_freedom} degrees of freedom.")
 
 # Plot the distribution using a KDE chart
 fig, ax = plt.subplots()
-sns.kdeplot(t_distribution_points, ax=ax, fill=False, color="blue", alpha=0.7,label = 't_distribution')
 sns.kdeplot(z_distribution, ax=ax, fill=False, color="red", alpha=0.7,label = 'z_distribution')
+sns.kdeplot(t_distribution_points, ax=ax, fill=False, color="blue", alpha=0.7,label = 't_distribution')
+# sns.kdeplot(chi_squared_points, ax=ax, fill=False, color="black", alpha=0.7,label = 'z_distribution')
+
 ax.legend()
 
 ax.set_title("t-Distribution Vs z-Distribution ")
